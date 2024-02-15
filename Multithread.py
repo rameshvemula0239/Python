47f943537986fdf0d473f21c926e0fe3dc31fede
rameshvemula0239 <137023560+rameshvemula0239@users.noreply.github.com>
2024-02-15 14:41:04 -0500

    Update Multithread.py

--- Multithread.py
+++ Multithread.py
@@ -54,7 +54,3 @@ def setup_threads():
 
 if _name_ == "_main_":
     setup_threads()
-
-In this setup, you'll need to replace 'path_to_passwords_file', 'path_to_text_file', and 'path_to_queue_elements_file' with the actual paths to the files that contain the inputs for each of the functions.
-
-You can use this structure to read inputs from files located in a separate sub-folder, as per your requirements. You would just need to make sure the paths you provide are correct.
